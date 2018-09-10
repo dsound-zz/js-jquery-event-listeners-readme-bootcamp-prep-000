75fce7fa6dee46abac12867eddf95a270cb8a5ac
demian sims <demiansims@gmail.com>
2018-09-10 15:22:33 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -24,4 +24,12 @@ function pressIt() {
     }
   })
 }
+function submitIt() {
+  $("form").on('submit', function() {
+    if ($( "input:first").val() === "correct") {
+      alert("Your form is going to be submitted now.");
+    }
+  }
+  
+
 
